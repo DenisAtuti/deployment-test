a407db82ca5b8391f97925022e3f1b93b5bd5f44
Dennis Atuti <atutidennis@gmail.com>
2020-11-28 20:36:03 +0300

    testing database

--- src/main/java/com/atutidennis/controller/BookingController.java
+++ src/main/java/com/atutidennis/controller/BookingController.java
@@ -1,13 +1,31 @@
 package com.atutidennis.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.atutidennis.model.Booking;
+import com.atutidennis.repository.BookingRepository;
+import com.atutidennis.service.BookingService;
 
 @Controller
 public class BookingController {
+	
+	@Autowired
+	private BookingService service;
+	
+	private BookingRepository repo;
 	
 	@GetMapping("/")
 	public String index() {
@@ -21,6 +39,54 @@ public class BookingController {
 	public String showForm(Model model) {
 		model.addAttribute("booking",new Booking());
 	    return "bookingPage";
+	}
+	@PostMapping("/posting")
+	public String saveOrder(@Valid @ModelAttribute("booking") Booking booking, BindingResult bindingResult,
+							RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return "bookingPage";
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Your booking is successfullly made, we will contact us soon");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		service.saveOrder(booking);
+		return "redirect:/bookingPage";
+		
+	}
+	
+	@GetMapping("/admin")
+	public String getData(Model model, @RequestParam(value = "page",required = false) Integer p, @RequestParam(defaultValue = "") String name) {
+		int perPage = 5;
+		int page = (p != null) ? p : 0;
+		Pageable pageable = PageRequest.of(page, perPage);
+		
+		Page<Booking> bookings = repo.findByUsernameLikeOrderByIdDesc(pageable,"%"+name+"%");
+		
+//		List <UserBookingModel> bookings = service.findByName(name);
+		int annualEarning = service.getAnnualEarning();
+		int monthyEarning = service.getMonthyEarning();
+		long count = service.getEntries();
+		int todayTask = service.getTodayTasks();
+		
+		model.addAttribute("annualEarning",annualEarning );
+		model.addAttribute("monthyEarning",monthyEarning );
+		model.addAttribute("count", count);
+		model.addAttribute("todayTask", todayTask);
+		model.addAttribute("bookings", bookings);
+		
+		long countBooking = repo.count();
+		double pageCount = Math.ceil((double)countBooking/(double)perPage);
+		
+		model.addAttribute("pageCount",(int)pageCount );
+		model.addAttribute("perPage", perPage);
+		model.addAttribute("countBooking", countBooking);
+		model.addAttribute("page", page);
+		
+		
+		
+		return "dashboard";
 	}
 
 }
--- src/main/java/com/atutidennis/service/BookingService.java
+++ src/main/java/com/atutidennis/service/BookingService.java
@@ -1,8 +1,109 @@
 package com.atutidennis.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.atutidennis.model.Booking;
+import com.atutidennis.repository.BookingRepository;
+
 
 @Service
 public class BookingService {
+	
+	@Autowired
+	private BookingRepository bookingRepository;
+	
+	public void saveOrder(Booking booking) {
+		bookingRepository.save(booking);
+	}
+	
+	public int getAnnualEarning() {
+		List<Booking> bookingList = bookingRepository.findAll();
+		ArrayList<Integer> hoursArray = new ArrayList<Integer>();
+				
+		bookingList.forEach(data -> {
+			LocalDate currentDate = LocalDate.now(); // 2016-06-17
+			int y = currentDate.getYear(); // 2016
+
+			
+			String bookingDate = data.getCleaningDate();
+			LocalDate date = LocalDate.parse(bookingDate);
+			
+			int bookingYear = date.getYear();
+			
+			if(bookingYear == y) {
+				hoursArray.add(Integer.parseInt(data.getHours()));
+				
+			}
+			
+		});
+		
+		int hours = 0;
+		for (int i = 0; i < hoursArray.size(); i++) {
+			hours = hours + hoursArray.get(i) ;
+		}
+
+		int totalHours = 25 * hours;
+		return totalHours;
+		
+	}
+	public int getMonthyEarning() {
+		List<Booking> bookingList = bookingRepository.findAll();
+		ArrayList<Integer> hoursArray = new ArrayList<Integer>();
+				
+		bookingList.forEach(data -> {
+			LocalDate currentDate = LocalDate.now(); // 2016-06-17
+			int mon = currentDate.getMonthValue();
+
+			
+			String bookingDate = data.getCleaningDate();
+			LocalDate date = LocalDate.parse(bookingDate);
+			
+			int bookingMonth = date.getMonthValue();			
+			if(bookingMonth == mon) {
+				hoursArray.add(Integer.parseInt(data.getHours()));
+			}
+		});
+		
+		int hours = 0;
+		for (int i = 0; i < hoursArray.size(); i++) {
+			hours = hours + hoursArray.get(i) ;
+		}	
+		int totalHours = 25 * hours;
+		
+		return totalHours;
+		
+	}
+	public long getEntries() {
+		return bookingRepository.count();
+		
+	}
+	
+	public int getTodayTasks() {
+		List<Booking> bookingList = bookingRepository.findAll();
+		ArrayList<Integer> todayTask = new ArrayList<Integer>();
+		bookingList.forEach(data -> {
+			LocalDate currentDate = LocalDate.now();
+			int todayDate = currentDate.getDayOfMonth();
+			
+			String bookingDate = data.getCleaningDate();
+			LocalDate date = LocalDate.parse(bookingDate);
+			int bookingDay = date.getDayOfMonth();
+			
+			if(todayDate == bookingDay) {
+				todayTask.add(data.getId());
+			}
+			
+		});
+		
+		return todayTask.size();
+		
+	}
+	
+
 
 }
